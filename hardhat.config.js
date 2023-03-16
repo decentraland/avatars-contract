dc66de7114760655e91041aebd198cde6b21be6a
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2023-03-16 08:08:48 -0500

    feat: Add script to deploy new controller (#47)
    * feat: Add script to deploy new controller
    
    * chore: EOL
    
    * chore: Add default network
    
    * chore: Add default rpc url for deploy
    
    * chore: Remove default network
    
    * chore: Rename fixture

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,6 +4,9 @@ require('babel-polyfill')
 require('decentraland-contract-plugins/dist/src/mana/tasks/load-mana')
 require('@nomiclabs/hardhat-truffle5')
 require('solidity-coverage')
+require('@nomiclabs/hardhat-etherscan')
+
+require('dotenv').config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -15,5 +18,14 @@ module.exports = {
         runs: 200,
       },
     },
+  },
+  networks: {
+    deploy: {
+      url: process.env.RPC_URL || 'http://some-rpc-url.org',
+      timeout: 600000,
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_KEY,
   },
 }
